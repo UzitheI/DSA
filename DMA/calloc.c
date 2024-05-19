db5c00409c876704d9a0153678acf7d3b9650c9f
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-05-19 11:04:55 +0545

    calloc for dma

---
+++ DMA/calloc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int n, i;
+    int *array;
+    printf("Enter the number of blocks you want to allocate:");
+    scanf("%d",&n );
+    array= (int*)calloc(n,sizeof(int ));
+
+    for(i=0;i<n;i++){
+        printf("a[%d]=",i);
+        scanf("%d",&array[i]);
+    }
+
+    for(i=0;i<n;i++){
+        printf("a[%d]=%d\t",i, array[i]);
+    }
+    return 0;
+}
